c569afa98e31235107168c86dd8154e1d49d8a04
tassyosilva <tassyoms22@gmail.com>
2025-05-13 09:42:37 -0400

    Mudança da mensagem padrão.

--- backend/processor.js
+++ backend/processor.js
@@ -6,10 +6,10 @@ const path = require('path');
 // Função para gerar a mensagem personalizada
 function gerarMensagem(nome, data, hora) {
     return `Olá, ${nome}!
-Você está recebendo uma intimação para comparecer à sede da Delegacia mais próxima de sua residência, até a data limite de ${data}, às ${hora}h, munido(a) de documento de identificação e deste MANDADO DE INTIMAÇÃO, para prestar esclarecimento sobre fato em apuração.
-${nome}, ao se apresentar, você deverá procurar o Delegado de Polícia e apresentar este MANDADO DE INTIMAÇÃO.
-A ausência injustificada poderá caracterizar crime de desobediência, previsto no artigo 330 do Código Penal.
-Em caso de dúvidas entre em contato através do número (númerodapolicia)
+O(A) Senhor(a) está recebendo uma intimação para comparecer à sede da Delegacia mais próxima de sua residência, até a data de ${data}, às ${hora}h, munido(a) de documento de identificação e deste MANDADO DE INTIMAÇÃO, para prestar esclarecimento sobre fato em apuração.
+${nome}, ao se apresentar, o(a) Senhor(a) deverá procurar o Delegado de Polícia e apresentar este MANDADO DE INTIMAÇÃO.
+Em caso de ausência, favor justificar junto ao cartório da Delegacia mais próxima, pois a ausência injustificada poderá caracterizar crime de desobediência, previsto no artigo 330 do Código Penal.
+Em caso de dúvidas, entre em contato através do número (95)99168-7209
 A confirmação da titularidade desta conta de WhatsApp pela Polícia Civil do Estado de Roraima pode ser conferida acessando o site da PCRR no link abaixo: 
 https://www.policiacivil.rr.gov.br/link/`;
 }
